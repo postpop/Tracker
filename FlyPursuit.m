b2e4ce467e8049771f3431b773c4e1ef161febf2
Jan Clemens <clemensjan@googlemail.com>
2014-01-15 18:00:31 -0500

    cleaned FLyPursuit

--- FlyPursuit.m
+++ FlyPursuit.m
@@ -9,7 +9,7 @@ classdef FlyPursuit < handle
       medianFrame, stdFrame, madFrame
       gmmStart, gmm
       arena, arenaCrop, arenaX, arenaY, boundsX, boundsY
-      
+      seNew, seOld, se10
       centroid, sigma, tracks, pathLabels
       
    end
@@ -33,7 +33,10 @@ classdef FlyPursuit < handle
          obj.tracks = zeros(obj.vr.NumberOfFrames, obj.nFlies,2);
          obj.pathLabels = zeros(obj.vr.NumberOfFrames, obj.nFlies);% track label for each centroid/sigma
          obj.pathLabels(1,:) = 1:obj.nFlies;% seed initial labels
-         
+         %
+         obj.seNew = strel('disk',10);
+         obj.seOld = strel('disk',10);
+         obj.se10 = strel('disk',10);
          
       end
       
@@ -70,9 +73,13 @@ classdef FlyPursuit < handle
          %obj.currentFrame = obj.getFrames(obj.frameIdx);
       end
       
-      function drawArena(obj)
+      
+      function drawArena(obj, frameNumber)
          %% draw arena and set initial positions
-         frame = obj.getFrames(1);
+         if nargin==1
+            frameNumber = 1;
+         end
+         frame = obj.getFrames(frameNumber);
          clf
          imagesc(frame);
          disp('please draw arena...')
@@ -102,8 +109,8 @@ classdef FlyPursuit < handle
       end
       
       function g = clusterFrame(obj, frame)
+         % create samples from frame and custer points
          [X, Y] = randp(single(frame), obj.nSamp, 1);
-         
          g0.mu = obj.gmmStart.mu;
          g0.Sigma = obj.gmmStart.Sigma;
          g0.PComponents = obj.gmmStart.PComponents;
@@ -114,39 +121,72 @@ classdef FlyPursuit < handle
          
       end
       
-      function gmm = clusterBW(obj)
-         se = strel('disk',10);
-         mask = imdilate(obj.currentFrame,se)>0;% get mask from current frame
-         [bw, Nconn] = bwlabel(mask);% find conn comps in mask
-         clusterGroup = diag(bw(round(fp.gmmStart.mu(:,2)), round(fp.gmmStart.mu(:,1))));% assign cluster center to conn comps
-         
-         clear mu
-         for g = 1:Nconn % for each conn comp
-            gFrame = obj.currentFrame;
-            gFrame(bw~=g) = 0;% set pixels outside the group to zero
-            gIdx = find(clusterGroup==g); % how many flies in the group?
-            gNflies = length(gIdx);
-            [X, Y] = randp(gFrame, gNflies*200, 1);% get samples for centroids in the group
-            hold on
-            if gNflies==1% if there's only one fly we simply calc the mean and cov of the points
-               mu(gIdx,:) = mean([X;Y]');
-               Sigma(:,:,gIdx) = cov([X;Y]');
-            else% otherwise we have to cluster
-               gmmStart = gmdistribution(obj.gmmStart.mu(gIdx,:), ...
-                                         obj.gmmStart.Sigma(:,:,gIdx),...
-                                         normalizeSum(obj.gmmStart.PComponents(gIdx)));
-               gmm = gmdistribution.fit([X;Y]',gNflies, 'Start',gmmStart);
-               mu(gIdx,:) = gmm.mu;
-               Sigma(:,:,gIdx) = gmm.Sigma;
+      
+      function gmm = clusterComponents(obj, bw, gmm0, frame, clusterGroup, Nconn)
+         % find connected components and cluster each separately - esp. uselfull for many flies, since
+         % it divides one hard problem into a couple of easier ones
+         mu = zeros(obj.nFlies,2);
+         Sigma = zeros(2,2,obj.nFlies);
+         cnt = 0;
+         while any(mu(:)==0) && cnt<3
+            for g = 1:Nconn % for each conn comp
+               gFrame = frame;
+               gFrame(bw~=g) = 0;% set pixels outside the group to zero
+               
+               gIdx = find(clusterGroup==g); % how many flies in the group?
+               gNflies = length(gIdx);
+               [X, Y] = randp(gFrame, gNflies*1000, 1);% get samples for centroids in the group
+               %                [X, Y] = sampleGrid(gFrame);% get samples for centroids in the group
+               %                X = X';
+               %                Y = Y';
+               
+               hold on
+               if gNflies==0
+                  disp(['region ' int2str(g) ' contains no flies.'])
+               elseif gNflies==1 % if there's only one fly we simply calc the mean and cov of the points
+                  mu(gIdx,:) = mean([X;Y],2);
+                  Sigma(:,:,gIdx) = cov([X;Y]');
+                  %                   S=ellipsefit(X,Y);
+                  %                   mu(gIdx,:) = [S.Xc, S.Yc];
+                  %                   cov([X;Y]')
+                  %                   ss = rotateVec2D([S.A.*[1 0]'], S.Phi)
+                  %                   ss = rotateVec2D([S.B.*[1 0]'], pi/2+S.Phi)
+                  
+                  %bwProps = regionprops(logical(bw),frame,{'WeightedCentroid'});
+                  %wcent = reshape([bwProps.WeightedCentroid],2,[]);
+               else % otherwise we cluster
+                  % get initial conditions from previous positions for the subset
+                  % of flies in the current region
+                  gmmStart.mu = gmm0.mu(gIdx,:);
+                  gmmStart.Sigma = gmm0.Sigma(:,:,gIdx);
+                  gmmStart.PComponents = normalizeSum(gmm0.PComponents(gIdx));
+                  % cluster
+                  try
+                     gmmTmp = gmdistribution.fit([X;Y]', gNflies, 'Start',gmmStart);
+                  catch ME
+                     disp(ME.getReport())
+                     try % if an error occurs, try regularization
+                        gmmTmp = gmdistribution.fit([X;Y]', gNflies, 'Start',gmmStart, 'Regularize',1);
+                     catch ME2 % if that does not help, start from scratch by ignoring initial conditions (might be risky...)
+                        disp(ME2.getReport())
+                        gmmTmp = gmdistribution.fit([X;Y]', gNflies, 'Replicates',100);
+                     end
+                  end
+                  % collect results over all connected components
+                  mu(gIdx,:) = gmmTmp.mu;
+                  Sigma(:,:,gIdx) = gmmTmp.Sigma;
+                  
+               end
+            end
+            if any(mu(:)==0)
+               disp('centroids at 0 - doing it again.')
+               cnt = cnt+1;
             end
          end
-         for i = 1:p.obj.nFlies
-            eigVal = eig(Sigma(:,:,i));
-            area(i) = pi*prod(sqrt(diag(eigVal)));
-         end
-         gmm = gmdistribution(mu, sigma, normalizeSum(area));
+         gmm.mu = mu;
+         gmm.Sigma = Sigma;
+         
       end
-      
       
       function getBackGround(obj, varargin)
          nFrames = varargin{1};
@@ -172,24 +212,87 @@ classdef FlyPursuit < handle
       end
       
       function frame = getForeGround(obj, frame, sense)
+         %frame = obj.getForeGround(frame, sense)
          if nargin==2
             sense = 5;
          end
          se = strel('disk',2);
          H = fspecial('gaussian',30,3);
          frame = single(frame);
-         frame = -(frame - obj.medianFrame);           % distance to background
-         frame = limit(frame,0, inf);                           % keep only pos deviations
+         frame = -(frame - obj.medianFrame);         % distance to background
+         frame = limit(frame,0, inf);                % keep only pos deviations
          lum = mean(frame(:));
-         frame = frame>lum*sense*(obj.madFrame);             % scale thres by mean luminance of the frame
+         frame = frame>lum*sense*(obj.madFrame);     % scale thres by mean luminance of the frame
+         
          frame = single(frame);
-         frame(~obj.arenaCrop(:)) = 0;                                 % delete everything outside arena
-         frame = medfilt2(frame,[3 3]);                  % remove specks
-         frame = imerode(frame, se);                      % erode...
-         frame = imfilter(frame, H, 'replicate');          % smooth
+         frame(~obj.arenaCrop(:)) = 0;               % delete everything outside arena
+         frame = medfilt2(frame,[3 3]);              % remove specks
+         frame = imerode(frame, se);                 % erode...
+         frame = imfilter(frame, H, 'replicate');    % smooth
+         %%
       end
       
+      function [bw, frame, clusterGroup, Nconn] = getForeGroundAdaptive(obj, oriFrame, oldFrame, oldCentroids, sense)
+         if nargin<5
+            sense = 10;
+         end
+         
+         lostFly = 1;
+         cnt = 0;
+         while ~isempty(lostFly) && cnt<50
+            frame = obj.getForeGround(oriFrame, sense);
+            % clean frame - remove specks that appear out of thin air...
+            oldMask = imdilate(logical(oldFrame), obj.seOld);
+            frame(~oldMask) = 0;% remove all pixels that are far away from the flies' previous positions
+            % find isolated groups of flies
+            mask = imdilate(logical(frame), obj.seNew);% get mask from current frame
+            [bw, Nconn] = bwlabel(mask);% find conn comps in mask
+            clusterGroup = diag(bw(...
+               limit(round(oldCentroids(:,2)),1,max(obj.boundsY)),...
+               limit(round(oldCentroids(:,1)),1,max(obj.boundsX))));% assign cluster center to conn comps
+            lostFly = find(clusterGroup==0);% who did we loose?
+            if ~isempty(lostFly)
+               disp(['   we lost the following members of the crew: ' mat2str(lostFly') '.'])
+               sense = 0.9*sense;
+               disp(['   going back with lower threshold (new thres = ' num2str(sense) ').'])
+               cnt = cnt+1;
+            end
+            %             subplot(221)
+            %             cla
+            %             imagesc(oriFrame)
+            %             hold on
+            %             %gscatter(oldCentroids(:,1), oldCentroids(:,2), p.pathLabels(f-1,:),cols,[],[],'off')
+            %             plot(oldCentroids(:,1), oldCentroids(:,2), '.k')
+            %             hold off
+            %             subplot(222)
+            %             cla
+            %             imagesc(mask-frame)
+            %             hold on
+            %             plot(oldCentroids(:,1), oldCentroids(:,2), '.r')
+            %             hold off
+            %             subplot(223)
+            %             cla
+            %             imagesc(bwlabel(mask))
+            %             hold on
+            %             plot(oldCentroids(:,1), oldCentroids(:,2), 'xk')
+            %             drawnow
+         end
+      end
       
+      function newLabels = assignCentroid2Track(obj, oldCentroid, newCentroid, oldLabels, maxDist)
+         if nargin<5
+            maxDist = 20;
+         end
+         C1 = squeeze(oldCentroid);
+         C2 = squeeze(newCentroid);
+         D = pdist2(C1, C2);
+         D(D>maxDist) = 1000*D(D>maxDist);% penalize jumps - works only partly
+         assign = munkres(D);
+         [idx, ~] = find(assign);
+         %%
+         newLabels = oldLabels(idx);% labels for each p.centroid
+         if ~all(newLabels), newLabels = obj.nFlies; end
+      end
       
       function playTrack(obj, history, offset)
          if nargin==2
